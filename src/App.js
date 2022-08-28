95a878b901f82202f625a187a1c45c17ed6a6f8e
muktadir hossain <muktadirhossainrabbi@gmail.com>
2022-08-29 02:02:13 +0600

    Routes declreared and git ignor added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import './App.css';
+import { Routes,Route,Link } from 'react-router-dom'
 import SignUp from './components/Sign up/SignUp'
+import Home from './components/home/Home';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
-    <div>
-      <SignUp />
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/signIn' element={<SignUp/>} />
+        <Route path='*' element={<PageNotFound/>} />
+      </Routes>
+    </>
   )
 }
 
